47d7a9190be14c3bcc7e5697bc82dd7ad4e49fd9
codingyq <zyqballuk@gmail.com>
2025-09-14 19:06:32 +0800

    Rename notebooks to consistent format

--- 08 FileReading Open _ 0914.py
+++ 08 FileReading Open _ 0914.py
@@ -1,5 +1,5 @@
 # Open & Close
-file = open('open.txt', 'r')
+file = open('08 FileReading Open _ 0914.txt', 'r')
 print(file.read())
 file.close()
     # r = read file
@@ -7,37 +7,37 @@ file.close()
     # a = append file
 
 # # readable() - boolean value to tell us if txt file can be read
-file = open('open.txt', 'r')
+file = open('08 FileReading Open _ 0914.txt', 'r')
 print(file.readable())
 file.close()
 
 
 # readline() - read the first line
-file = open('open.txt', 'r')
+file = open('08 FileReading Open _ 0914.txt', 'r')
 print(file.readline())
 file.close()
 
 
 # readline() *n - reading multiple lines: like moving a cursor onto the next line - read line one by one.
-file = open('open.txt', 'r')
+file = open('08 FileReading Open _ 0914.txt', 'r')
 print(file.readline())
 print(file.readline())
 file.close()
 
 
 # readlines() -  take all the lines in the file and make them array
-file = open('open.txt', 'r')
+file = open('08 FileReading Open _ 0914.txt', 'r')
 print(file.readlines())   # taking each line and put them as array
 file.close()
 
 # readlines()[1] -  access specific line by refer to its index in the array
-file = open('open.txt', 'r')
+file = open('08 FileReading Open _ 0914.txt', 'r')
 print(file.readlines()[3])
 file.close()
 
 
 # readlines() function with for loop
-file = open('open.txt', 'r')
+file = open('08 FileReading Open _ 0914.txt', 'r')
 for sentence in file.readlines():    # file.readlines() is an array.
     print(sentence)
 file.close()
