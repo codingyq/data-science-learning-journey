70b5bfb56162dc366b1b0b0cbc0b01ff78f1da98
codingyq <zyqballuk@gmail.com>
2025-09-20 15:31:11 +0800

    09 Writing and Appending Files _ 0914

--- freecodecamp-python/notebooks/09 Writing and Appending Files _ 0914.py
+++ freecodecamp-python/notebooks/09 Writing and Appending Files _ 0914.py
@@ -3,7 +3,7 @@
 
 
 # Section A: appending another line in the txt file
-file = open("08 FileReading Open _ 0914.txt", "a")
+file = open("../samples/08 FileReading Open _ 0914.txt", "a")
 file.write("Adding additional information - new entry")     # writing something in the end of the file
 # now check the file it will show a new line
 # Need to be careful ! Because if run twice and it will append 2 times. - without a \n
@@ -26,12 +26,12 @@ file.close()
 # Section C: create a new file
 
 # use "w" to create a new file - txt as extension
-file = open("09 Writing and Appending Files _ 0914.txt","w")
+file = open("../samples/09 Writing and Appending Files _ 0914.txt", "w")
 file.write("Hi, this is a new file for 09 writing file")
 file.close()
 
 # use "w" to create a new file - html as extension
-file = open("09 Writing and Appending Files _ 0914.html","w")
+file = open("../samples/09 Writing and Appending Files _ 0914.html", "w")
 file.write("<p>This is HTML</p>")                                # <p> paragraph - HTML language
 file.close()
 
